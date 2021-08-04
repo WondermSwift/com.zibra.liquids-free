d6450d88d8c453a666631d4ba71e8331417bb1a2
Yurii-Tor <yurii.tor@bofh.od.ua>
2021-08-04 11:45:38 +0300

    chore: made Info tab links dynamic

--- Editor/Configs/ZibraAIPackage.cs
+++ Editor/Configs/ZibraAIPackage.cs
@@ -5,6 +5,10 @@ namespace com.zibra.liquid.Editor
 		public const string ZibraAiSupportEmail = "support@zibraai.com";
 		public const string ZibraAiCeoEMail = "ceo@zibraai.com";
 		public const string ZibraAiWebsiteRootUrl = "https://zibra.ai/";
+		public const string ZibraAiLinkedinUrl = "https://www.linkedin.com/company/zibra-ai/";
+		public const string ZibraAiFBUrl = "https://www.facebook.com/zibraAI/";
+		public const string ZibraAiYoutubeUrl = "https://www.youtube.com/channel/UC2AkLxzSIFTB1E2lvNs73xg";
+		public const string ZibraAiDiscordUrl = "https://discord.gg/G4FNajB3D9";
 
 		public const string PackageName = "com.zibra.liquids-free";
 		public const string DisplayName = "Zibra AI - Liquids";
--- Editor/UIToolkit/AboutTab/AboutTab.cs
+++ Editor/UIToolkit/AboutTab/AboutTab.cs
@@ -1,13 +1,42 @@
-﻿#if UNITY_2019_4_OR_NEWER
+﻿using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+#if UNITY_2019_4_OR_NEWER
 namespace com.zibra.liquid.Editor
 {
     class AboutTab : BaseTab
     {
         public AboutTab()
             : base($"{ZibraAIPackage.UIToolkitPath}/AboutTab/AboutTab")
-        
-        {
 
+        {
+            var supportMailHyperlink = this.Q<Hyperlink>("supportEmail");
+            supportMailHyperlink.Link = "mailto:" + ZibraAIPackage.ZibraAiSupportEmail;
+            
+            var supportMailLabel = this.Q<Label>("supportEmailText");
+            supportMailLabel.text = ZibraAIPackage.ZibraAiSupportEmail;
+            
+            var ceoMailHyperlink = this.Q<Hyperlink>("ceoEmail");
+            ceoMailHyperlink.Link = "mailto:" + ZibraAIPackage.ZibraAiCeoEMail;
+            
+            var ceoMailLabel = this.Q<Label>("ceoEmailText");
+            ceoMailLabel.text = ZibraAIPackage.ZibraAiCeoEMail;
+            
+            var linkedinHyperlink = this.Q<Hyperlink>("LinkedinElement");
+            linkedinHyperlink.Link = ZibraAIPackage.ZibraAiLinkedinUrl;
+            
+            var fbHyperlink = this.Q<Hyperlink>("FacebookElement");
+            fbHyperlink.Link = ZibraAIPackage.ZibraAiFBUrl;
+            
+            var youtubeHyperlink = this.Q<Hyperlink>("YoutubeElement");
+            youtubeHyperlink.Link = ZibraAIPackage.ZibraAiYoutubeUrl;
+            
+            var discordHyperlink = this.Q<Hyperlink>("DicordElement");
+            discordHyperlink.Link = ZibraAIPackage.ZibraAiDiscordUrl;
+            
+            var logoHyperlink = this.Q<Hyperlink>("LogoElement");
+            logoHyperlink.Link = ZibraAIPackage.ZibraAiWebsiteRootUrl;
         }
     }
 }
